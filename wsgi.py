5b0d93ae0a1973c2d8045221118a839e9f20115b
Marek Stępniowski <marek@stepniowski.com>
2013-05-11 00:30:48 +0200

    html pre

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,13 @@
 import redis
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 @app.route("/")
-def hello():
-    return r.get('board')
+def board():
+    return render_template('board.html', board=r.get('board'))
 
 
 if __name__ == '__main__':
