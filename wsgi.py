fe7c6d4af04b287be346d2c6a4c5dc06a8eb590e
Marek Stępniowski <marek@stepniowski.com>
2013-05-10 20:32:27 +0200

    Return board.

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,13 @@
+import redis
 from flask import Flask
 
 app = Flask(__name__)
 
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return r.get('board')
 
 
 if __name__ == '__main__':
