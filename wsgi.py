e01dc82a27b5aca31e31904d879a43f812c5755f
Marek Stępniowski <marek@stepniowski.com>
2013-05-11 17:15:23 +0200

    Nicer!

--- wsgi.py
+++ wsgi.py
@@ -6,11 +6,44 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
+SCRIPT = """\
+import sys, random
+
+WIDTH, HEIGHT = 80, 60
+
+# Change here!
+# ------------
+# Board is a list of strings (board rows), where:
+#  - o = apple
+#  - # = snake
+#  - H = head of our snake
+#  - . = empty field
+#
+# Move should return 'left', 'right', 'up' or 'down'
+def move(board):
+    return random.choice(['left', 'right', 'up', 'down'])
+
+# Don't touch (unless you know what you're doing :-))
+while True:
+    board = [sys.stdin.readline() for _ in range(HEIGHT)]
+    sys.stdout.write(move(board))
+    sys.stdout.write('\n')
+"""
 
 @app.route('/')
 @app.route('/snake/<key>')
 def board(key=None):
-    return render_template('board.html', board=r.get('board'), key=key or '')
+    snake_name = 'Annonymous'
+    snake_code = SCRIPT
+
+    if key is not None:
+        snake_name = r.get('snake:%s:name' % key) or ''
+        snake_code = r.get('snake:%s:code' % key) or SCRIPT
+    return render_template('board.html',
+                           board=r.get('board'),
+                           key=key or '',
+                           name=snake_name,
+                           code=snake_code)
 
 
 @app.route('/board')
@@ -30,6 +63,9 @@ def reload_code():
                                          request.form['slave_name'],
                                          request.form['slave_code'])
     r.rpush('commands', command)
+    r.set('snake:%s:name' % request.form['slave_id'], request.form['slave_name'])
+    r.set('snake:%s:code' % request.form['slave_id'], request.form['slave_code'])
+
     return redirect(url_for('board', key=request.form['slave_id']))
 
 
