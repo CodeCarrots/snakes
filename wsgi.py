adeb79e08126fed70ae8ca1c7695d8429f9fb6e4
Marek Stępniowski <marek@stepniowski.com>
2013-05-12 23:29:08 +0200

    Show unified file log in the application.

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 import redis
 import json
 from flask import Flask, render_template, request, redirect, url_for
-
+from flask import Response
 
 app = Flask(__name__)
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
@@ -30,6 +30,36 @@ while True:
     sys.stdout.write(move(board))
     sys.stdout.write('\\n')
 """
+
+def tail( f, window=20 ):
+    BUFSIZ = 1024
+    f.seek(0, 2)
+    bytes = f.tell()
+    size = window
+    block = -1
+    data = []
+    while size > 0 and bytes > 0:
+        if (bytes - BUFSIZ > 0):
+            # Seek back one whole BUFSIZ
+            f.seek(block*BUFSIZ, 2)
+            # read BUFFER
+            data.append(f.read(BUFSIZ))
+        else:
+            # file too small, start from begining
+            f.seek(0,0)
+            # only read what was not read
+            data.append(f.read(bytes))
+        linesFound = data[-1].count('\n')
+        size -= linesFound
+        bytes -= BUFSIZ
+        block -= 1
+    return '\n'.join(''.join(data).splitlines()[-window:])
+
+
+@app.route('/log')
+def log():
+    return Response(tail(file('/srv/webapps/snakes/log/judge.log'), 1000), mimetype='text/plain')
+
 
 @app.route('/')
 @app.route('/snake/<key>')
